2c7eb8b888168c5be985e3ed95ef673748990bf7
helloMan <22980937@qq.com>
2022-11-14 10:04:26 +0800

    fix: fix import

--- src/layout/Horizontal/index.vue
+++ src/layout/Horizontal/index.vue
@@ -5,7 +5,7 @@ import VSidebar from '../sidebar/index.vue'
 import TabViews from '../tabViews/index.vue'
 import RightPane from '../components/RightPane/index.vue'
 import { useStore } from '@/store'
-import { computed } from 'vue-demi'
+import { computed } from 'vue'
 
 const store = useStore()
 
