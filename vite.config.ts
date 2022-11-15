46662f2491aa68575380ec41c87ac99dfe99266d
helloMan <22980937@qq.com>
2022-11-15 10:10:12 +0800

    chore: merge

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { viteMockServe } from 'vite-plugin-mock'
 import { resolve } from 'path'
 import { ServerConf } from './src/config'
-import ElementPlus from 'unplugin-element-plus/vite'
 
 const { host, port } = ServerConf
 
